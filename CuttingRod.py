a440dab180362c4fd7b25dfd341885504909e4c2
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2020-07-12 22:43:24 +0530

    Update CuttingRod.py

--- CuttingRod.py
+++ CuttingRod.py
@@ -1,10 +1,11 @@
 def rodCutting(price, n): 
     val = [0 for x in range(n+1)] 
     val[0] = 0
-    INT_MIN = -32767
     
+    #For each i make a max_vala nd store it in val array
     for i in range(1, n+1): 
-        max_val = INT_MIN 
+        #Find max val by using the already present values for smaller numbers in the val array
+        max_val = -1
         for j in range(i): 
              max_val = max(max_val, price[j] + val[i-j-1]) 
         val[i] = max_val 
